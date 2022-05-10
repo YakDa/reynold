039e5043aa4b8a8adab3fa0754d2aec1231c8b6e
mingda.cai <mingda.cai@envsion-digital.com>
2022-05-10 11:39:34 +0800

    test clean function

--- reynold_logger_test.go
+++ reynold_logger_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func TestNewPerfSingleton(t *testing.T) {
+func TestAddData(t *testing.T) {
 	AddData("key1", PerfData{
 		Message:   "This is a test",
 		Timestamp: time.Now(),
@@ -17,7 +17,7 @@ func TestNewPerfSingleton(t *testing.T) {
 	assert.Equal(t, 1, Count(), "One logger should be added")
 }
 
-func TestPerfLoggers_Ordering(t *testing.T) {
+func TestOrdering(t *testing.T) {
 	AddData("key1", PerfData{
 		Message:   "This is a test",
 		Timestamp: time.Now(),
@@ -38,4 +38,28 @@ func TestPerfLoggers_Ordering(t *testing.T) {
 	assert.Equal(t, "key2", perfLoggers.list[1])
 	assert.Equal(t, "key3", perfLoggers.list[2])
 	log.Println(String())
+}
+
+func TestClean(t *testing.T) {
+	AddData("key1", PerfData{
+		Message:   "This is a test",
+		Timestamp: time.Now(),
+		Duration:  900,
+	})
+	AddData("key2", PerfData{
+		Message:   "This is a test",
+		Timestamp: time.Now(),
+		Duration:  900,
+	})
+	AddData("key3", PerfData{
+		Message:   "This is a test",
+		Timestamp: time.Now(),
+		Duration:  900,
+	})
+
+	assert.Equal(t, 3, len(perfLoggers.hash), "length should be 3")
+	assert.Equal(t, 3, len(perfLoggers.list), "length should be 3")
+
+	Clean()
+	assert.Equal(t, 0, len(perfLoggers.hash), "logs should be cleaned")
 }
